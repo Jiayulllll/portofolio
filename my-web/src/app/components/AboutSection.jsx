d29d948fa5753f7fc54ebbc1cc71b1f4746b8e72
Trista Liu <jliu2217@gmail.com>
2024-05-23 15:39:31 +1000

    change app name

--- my-web/src/app/components/AboutSection.jsx
+++ my-web/src/app/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const AboutSection = () => {
   return (
-    <section className="text-white">
+    <section id="about" className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 ">
         <Image src="/images/about.jpg" alt="" width={500} height={500} />
 
